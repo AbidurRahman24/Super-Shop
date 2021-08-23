24597153b9441fe1adf6fc52024aa365e5b2735a
Niloy-Sumon <niloyabidur@gmail.com>
2021-08-23 16:20:59 +0600

    Readme file edit

--- src/index.js
+++ src/index.js
@@ -4,18 +4,18 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import 'bootstrap/dist/css/bootstrap.min.css';
-// import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
-// import Loader from "react-loader-spinner";
+import "react-loader-spinner/dist/loader/css/react-spinner-loader.css";
+import Loader from "react-loader-spinner";
 
 ReactDOM.render(
   <React.StrictMode>
-    {/* <Loader
+     <Loader
         type="Puff"
         color="#00BFFF"
         height={100}
         width={100}
         timeout={3000} //3 secs
-      /> */}
+      /> 
     <App />
   </React.StrictMode>,
   document.getElementById('root')
